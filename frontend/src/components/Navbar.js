cf7a5fdbb729be278a2f3c220c437b769d444df1
Joseph Jarlos <jarlosjoseph@gmail.com>
2023-11-30 21:57:10 +0800

    Added login and logout hooks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
+
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
 
   return (
     <header>
@@ -9,10 +18,18 @@ const Navbar = () => {
           <h1>Blog App</h1>
         </Link>
         <nav>
+          {user && (
+          <div>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+          )}
+          {!user && (
           <div>
             <Link to ="/login">Login</Link>
             <Link to ="/signup">Signup</Link>
           </div>
+          )}
         </nav>
       </div>
     </header>
